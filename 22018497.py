27c979c2944ced2c36a58247d04e1994cced753b
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-04-17 12:51:37 +0100

    Add files via upload

---
+++ 22018497.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 12:22:18 2023
+
+@author: aaron
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
