e09cc598cee8fa0e5dbe0e0acf5ad56b812104e3
Aaron M Joseph <125889154+aaronmj7@users.noreply.github.com>
2023-05-02 23:57:51 +0100

    Add files via upload

--- 22018497.py
+++ 22018497.py
@@ -9,8 +9,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# setiing seaborn theme
-sns.set_theme()
 
 # reading from csv
 df = pd.read_csv('Electricity_Production_By_Source.csv')
@@ -29,6 +27,17 @@ order = [0, 1, 2, 3, 7, 4, 9, 6, 8, 5]
 df = df[[df.columns[i] for i in order]]
 
 
+# plotting with GridSpec
+fig = plt.figure()
+gs = plt.GridSpec(2, 3, wspace=0.4, hspace=0.3)
+
+# seting seaborn theme
+sns.set_theme()
+
+# common kwargs
+kwargs = {'cmap': 'RdYlGn', 'legend': False}
+
+
 # plot 1
 
 # selecting world data only
@@ -41,27 +50,27 @@ df_wrld.set_index("Year", inplace=True)
 df_wrld_percent = df_wrld.apply(lambda x: round((x/sum(x)*100), 2), axis=1)
 
 # plotting area plot
-df_wrld_percent.plot(kind='area', colormap='RdYlBu')
-plt.show()
+ax00 = fig.add_subplot(gs[0, 0])
+df_wrld_percent.plot(kind='area', ax=ax00, **kwargs)
 
 # plot 2
 
 # plotting pie chart
-df_wrld.loc[2020].plot(kind='pie', autopct='%1.0f%%', colormap='RdYlBu')
-plt.show()
+ax01 = fig.add_subplot(gs[0, 1])
+df_wrld.loc[2020].plot(kind='pie', autopct='%1.0f%%', ax=ax01, **kwargs)
 
 # plot 3
 
 # plotting line plot
-df_wrld_percent.plot()
-plt.show()
+ax02 = fig.add_subplot(gs[0, 2])
+df_wrld_percent.loc[1990:].plot(ax=ax02, **kwargs)
 
 # plot 4
 
 # selecting Year 2020 only
 df_2020 = df[df['Year'] == 2020]
 # dropping columns that are not needed
-df_2020.drop("Year", axis=1, inplace=True)
+df_2020.drop('Year', axis=1, inplace=True)
 # setting index
 df_2020.set_index("Entity", inplace=True)
 # adding Total column
@@ -75,12 +84,15 @@ df_2020_sample.drop(['World', 'EU27+1', 'EU-27'], inplace=True)
 df_2020_sample.drop('Total', axis=1, inplace=True)
 
 # plotting horizontal bar plot
-df_2020_sample[::-1].plot(kind='barh', stacked=True, colormap='RdYlBu')
-plt.show()
+ax10 = fig.add_subplot(gs[1, 0])
+df_2020_sample[::-1].plot(kind='barh', stacked=True, ax=ax10, **kwargs)
 
 # plot 5
-
+'''
 # plotting pie charts
-df_2020_sample[:5].T.plot(kind='pie', subplots=True, autopct='%1.0f%%',
-                          legend=False, colormap='RdYlBu')
+ax11 = fig.add_subplot(gs[1, 1])
+df_2020_sample[:4].T.plot(kind='pie', subplots=True, layout=(2, 2), ax=ax11,
+                          **kwargs)
+'''
+# show the plot
 plt.show()
